18dceb23a87c6b70adca869478a8b5732b69911d
hogebein <tatsugi9682@gmail.com>
2024-10-22 16:58:30 +0900

    new eval settings

--- zsceval/scripts/overcooked/shell/train_bias_agents/train_bias_agents.sh
+++ zsceval/scripts/overcooked/shell/train_bias_agents/train_bias_agents.sh
@@ -129,5 +129,5 @@ do
     --cnn_layers_params "32,3,1,1 64,3,1,1 32,3,1,1" --use_recurrent_policy \
     --use_proper_time_limits \
     --save_interval 25 --log_interval 10 --use_eval --eval_interval 20 --n_eval_rollout_threads 20 \
-    --wandb_name "hogebein" \
+    --wandb_name "hogebein"
 done

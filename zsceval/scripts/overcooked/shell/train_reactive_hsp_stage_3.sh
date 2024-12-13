73d2419c7ca3a1d3602ac6aee6591c397df6f479
hogebein <tatsugi9682@gmail.com>
2024-12-14 04:11:27 +0900

    fixes

--- zsceval/scripts/overcooked/shell/train_reactive_hsp_stage_3.sh
+++ zsceval/scripts/overcooked/shell/train_reactive_hsp_stage_3.sh
@@ -10,7 +10,7 @@ if [[ "${layout}" == "random0" || "${layout}" == "random0_medium" || "${layout}"
 else
     version="new"
 fi
-if [[ ${population_size} == 12 ]]; then
+if [[ ${population_size} == 10 ]]; then
     entropy_coefs="0.2 0.05 0.01"
     entropy_coef_horizons="0 2.5e7 5e7"
     if [[ "${layout}" == "small_corridor" ]]; then

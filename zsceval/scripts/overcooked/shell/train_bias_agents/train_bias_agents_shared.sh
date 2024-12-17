32d48cab3f55099efb5bfd41488fd7b867d3ac22
hogebein <tatsugi9682@gmail.com>
2024-12-17 19:52:26 +0900

    fixes

--- zsceval/scripts/overcooked/shell/train_bias_agents/train_bias_agents_shared.sh
+++ zsceval/scripts/overcooked/shell/train_bias_agents/train_bias_agents_shared.sh
@@ -75,7 +75,16 @@ if [[ "${weight_pattern}" == "all" ]]; then
     seed_begin=1
     seed_max=124
 elif [[ "${weight_pattern}" == "plate_placed" ]]; then
-    w0="0,0,0,0,0,0,0,0,-3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0"
+    w0="0,0,0,0,0,0,0,0,-3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3.5,0,0,0,0,0,0"
+    seed_begin=1
+    seed_max=5
+
+    reward_shaping_horizon="1e7"
+    num_env_steps="1e7"
+
+    exp="hsp_plate_placement_shared-${stage}"
+elif [[ "${weight_pattern}" == "plate_placed_i" ]]; then
+    w0="0,0,0,0,0,0,0,0,-3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.04,0,0"
     seed_begin=1
     seed_max=5
 
@@ -85,7 +94,7 @@ elif [[ "${weight_pattern}" == "plate_placed" ]]; then
     exp="hsp_plate_placement_shared-${stage}"
 
 elif [[ "${weight_pattern}" == "plate_place" ]]; then
-    w0="0,0,0,0,0,0,0,0,-3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3.5,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+    w0="0,0,0,0,0,0,0,0,-3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
     seed_begin=6
     seed_max=10
 
@@ -106,7 +115,7 @@ else
     seed_max=72
 fi
 
-
+rollout_threads=100
 
 
 echo "seed_max is ${seed_max}:"
@@ -117,7 +126,7 @@ do
     if "${use_base_shaping_r}"; then
 
         python train/train_bias_agent.py --env_name ${env} --algorithm_name ${algo} --experiment_name "${exp}" --layout_name ${layout} --num_agents ${num_agents} \
-        --seed ${seed} --n_training_threads 2 --n_rollout_threads 100 --dummy_batch_size 2 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
+        --seed ${seed} --n_training_threads 2 --n_rollout_threads ${rollout_threads} --dummy_batch_size 2 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
         --overcooked_version ${version} \
         --ppo_epoch 15 --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
         --use_hsp --w0 ${w0} --w1 ${w1} --random_index \
@@ -132,7 +141,7 @@ do
     else
 
         python train/train_bias_agent.py --env_name ${env} --algorithm_name ${algo} --experiment_name "${exp}" --layout_name ${layout} --num_agents ${num_agents} \
-        --seed ${seed} --n_training_threads 2 --n_rollout_threads 100 --dummy_batch_size 2 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
+        --seed ${seed} --n_training_threads 2 --n_rollout_threads ${rollout_threads} --dummy_batch_size 2 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
         --overcooked_version ${version} \
         --ppo_epoch 15 --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
         --use_hsp --w0 ${w0} --w1 ${w1} --random_index \

d75ae7fcd977307a2c3e187c23691c86e1128634
hogebein <tatsugi9682@gmail.com>
2025-01-12 02:29:18 +0900

    fixes

--- zsceval/scripts/extract_models/extract_bias_agents_models.py
+++ zsceval/scripts/extract_models/extract_bias_agents_models.py
@@ -50,11 +50,11 @@ def extract_sp_S1_models(layout, exp, env="Overcooked"):
                 continue
             i = run.config["seed"]
             history = run.history()
-            history = history[["_step", "ep_sparse_r"]]
+            history = history[["_step", "ep_shaped_r"]]
             steps = history["_step"].to_numpy().astype(int)
-            ep_sparse_r = history["ep_sparse_r"].to_numpy()
-            final_ep_sparse_r = np.mean(ep_sparse_r[-5:])
-            logger.info(f"hsp{i} Run: {run_id} Seed: {run.config['seed']} Return {final_ep_sparse_r}")
+            ep_shaped_r = history["ep_shaped_r"].to_numpy()
+            final_ep_shaped_r = np.mean(ep_shaped_r[-5:])
+            logger.info(f"hsp{i} Run: {run_id} Seed: {run.config['seed']} Return {final_ep_shaped_r}")
             seeds.add(run.config["seed"])
             files = run.files()
             actor_pts = [f for f in files if f.name.startswith("actor")]
@@ -65,33 +65,33 @@ def extract_sp_S1_models(layout, exp, env="Overcooked"):
             max_steps = max(steps)
 
             new_steps = [steps[0]]
-            new_ep_sparse_r = [ep_sparse_r[0]]
-            for s, er in zip(steps[1:], ep_sparse_r[1:]):
+            new_ep_shaped_r = [ep_shaped_r[0]]
+            for s, er in zip(steps[1:], ep_shaped_r[1:]):
                 l_s = new_steps[-1]
-                l_er = new_ep_sparse_r[-1]
+                l_er = new_ep_shaped_r[-1]
                 for w in range(l_s + 1, s, 100):
                     new_steps.append(w)
-                    new_ep_sparse_r.append(l_er + (er - l_er) * (w - l_s) / (s - l_s))
+                    new_ep_shaped_r.append(l_er + (er - l_er) * (w - l_s) / (s - l_s))
             steps = new_steps
-            ep_sparse_r = new_ep_sparse_r
+            ep_shaped_r = new_ep_shaped_r
 
             # select checkpoints
             selected_pts = dict(mid=-1, final=max_steps)
-            mid_ep_sparse_r = final_ep_sparse_r / 2
+            mid_ep_shaped_r = final_ep_shaped_r / 2
             min_delta = 1e9
-            for s, score in zip(steps, ep_sparse_r):
-                if min_delta > abs(mid_ep_sparse_r - score):
-                    min_delta = abs(mid_ep_sparse_r - score)
+            for s, score in zip(steps, ep_shaped_r):
+                if min_delta > abs(mid_ep_shaped_r - score):
+                    min_delta = abs(mid_ep_shaped_r - score)
                     selected_pts["mid"] = s
 
             selected_pts = {k: int(v / max_steps * max_actor_versions) for k, v in selected_pts.items()}
-            sparse_r_dict = dict(init=0, mid=mid_ep_sparse_r, final=final_ep_sparse_r)
+            shaped_r_dict = dict(init=0, mid=mid_ep_shaped_r, final=final_ep_shaped_r)
             for tag, exp_version in selected_pts.items():
                 version = actor_versions[0]
                 for actor_version in actor_versions:
                     if abs(exp_version - version) > abs(exp_version - actor_version):
                         version = actor_version
-                logger.info(f"hsp{i}: {tag} Expected: {exp_version} {sparse_r_dict[tag]} Found: {version}")
+                logger.info(f"hsp{i}: {tag} Expected: {exp_version} {shaped_r_dict[tag]} Found: {version}")
 
                 actor_pts = []
                 if(run.config["share_policy"]):

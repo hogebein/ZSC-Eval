56a01b8637d8b33ba668aeed912bf6c3b05a66b3
Ta183 <47871855+hogebein@users.noreply.github.com>
2025-01-25 19:47:16 +0900

    fix

--- zsceval/envs/overcooked_new/Overcooked_Env.py
+++ zsceval/envs/overcooked_new/Overcooked_Env.py
@@ -1250,8 +1250,12 @@ class Overcooked(gym.Env):
             info["episode"]["ep_hidden_r_by_agent"] = self.cumulative_hidden_r
             info["episode"]["ep_utility_r_by_agent"] = self.cumulative_utility_r
             info["episode"]["ep_shaped_r_by_agent"] = self.cumulative_shaped_r
-            info["episode"]["ep_utility_r"] = sum(self.cumulative_utility_r)
+            info["episode"]["ep_sparse_r_by_agent"] = self.cumulative_sparse_r
+
+            info["episode"]["ep_sparse_r"] = sum(self.cumulative_sparse_r)
             info["episode"]["ep_shaped_r"] = sum(self.cumulative_shaped_r)
+            info["episode"]["ep_utility_r"] = sum(self.cumulative_utility_r)
+            info["episode"]["ep_hidden_r"] = sum(self.cumulative_hidden_r)
             info["bad_transition"] = True
         else:
             info["bad_transition"] = False

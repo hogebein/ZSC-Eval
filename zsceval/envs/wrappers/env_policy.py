63a5cd76e8ea18e9155eda4946e07b7eed3514aa
Ta183 <47871855+hogebein@users.noreply.github.com>
2025-01-27 07:08:44 +0900

    fix

--- zsceval/envs/wrappers/env_policy.py
+++ zsceval/envs/wrappers/env_policy.py
@@ -152,18 +152,14 @@ class PartialPolicyEnv:
             elif _utility[18] > 0:
                 # Complain when the opponent has taken a plate
                 log = [i["pickup_tomato_from_T"] for i in _infos_buffer[agent_id^1]]
-                logger.debug("45")
-                if sum(log) >= 1:
-                    
+                if sum(log) >= 1:      
                     return True
                 else:
                     return False
             elif _utility[19] > 0:
                 # Complain when the opponent has taken a plate
                 log = [i["pickup_onion_from_O"] for i in _infos_buffer[agent_id^1]]
-                logger.debug("46")
                 if sum(log) >= 1:
-                    
                     return True
                 else:
                     return False

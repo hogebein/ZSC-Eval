e099c8415b855d28b6799396db6fffb5728d2fde
Ta183 <47871855+hogebein@users.noreply.github.com>
2025-01-29 03:00:18 +0900

    debug

--- zsceval/envs/wrappers/env_policy.py
+++ zsceval/envs/wrappers/env_policy.py
@@ -134,7 +134,7 @@ class PartialPolicyEnv:
                 # P : Agent that likes to place plates by itsself 
                 if _utility[31] > 0:
                     # Complain when the opponent places a plate
-                    dishes_placed_log = [i["put_dish_on_X"] for i in _infos_buffer[agent_id]]
+                    dishes_placed_log = [i["put_dish_on_X"] for i in _infos_buffer[agent_id^1]]
                     if sum(dishes_placed_log) >= 1:
                         #logger.debug(dishes_placed_log)
                         return True
@@ -143,7 +143,7 @@ class PartialPolicyEnv:
                 # F : Agent that likes plates placed on the counter
                 elif _utility[51] > 0:
                     # Complain when the opponent has taken a plate
-                    dishes_recieved_log = [i["SOUP_PICKUP"] for i in _infos_buffer[agent_id]]
+                    dishes_recieved_log = [i["SOUP_PICKUP"] for i in _infos_buffer[agent_id^1]]
                     if sum(dishes_recieved_log) >= 1:
                         #logger.debug(dishes_recieved_log)
                         return True
@@ -155,8 +155,9 @@ class PartialPolicyEnv:
                 # CASE ONION_TOMATO
                 if _utility[18] > 0:
                     # Complain when the opponent has taken a plate
-                    log = [i["pickup_tomato_from_T"] for i in _infos_buffer[agent_id^1]]
-                    logger.debug(sum(log))
+                    log = [i["pickup_tomato_from_T"] for i in _infos_buffer[agent_id]]
+                    if sum(log) == 1:
+                      logger.debug(sum(log))
                     #logger.debug(agent_id)
                     if sum(log) >= 1:
                         return True
@@ -164,7 +165,7 @@ class PartialPolicyEnv:
                         return False
                 elif _utility[19] > 0:
                     # Complain when the opponent has taken a plate
-                    log = [i["pickup_onion_from_O"] for i in _infos_buffer[agent_id^1]]
+                    log = [i["pickup_onion_from_O"] for i in _infos_buffer[agent_id]]
                     logger.debug(sum(log))
                     #logger.debug(agent_id)
                     if sum(log) >= 1:
@@ -219,8 +220,8 @@ class PartialPolicyEnv:
                 else:
                     agent_diffs = {k:0 for k in agent_infos.keys()}
                     for key in agent_diffs.keys():
-                        #if key=="pickup_onion_from_O":
-                        #    logger.debug(agent_infos[key] - self.infos_previous[a][key])
+                        if key=="pickup_onion_from_O" and agent_infos[key] - self.infos_previous[a][key] == 1:
+                            logger.debug(agent_infos[key] - self.infos_previous[a][key])
                         agent_diffs[key] = agent_infos[key] - self.infos_previous[a][key]
                     self.infos_buffer[a].append(agent_diffs)
                     self.infos_previous[a] = agent_infos.copy()

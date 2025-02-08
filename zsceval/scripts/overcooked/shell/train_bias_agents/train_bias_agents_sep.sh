f2def57515545350a1325704857f6494fc5e5a75
Ta183 <47871855+hogebein@users.noreply.github.com>
2025-02-09 02:20:17 +0900

    fix

--- zsceval/scripts/overcooked/shell/train_bias_agents/train_bias_agents_sep.sh
+++ zsceval/scripts/overcooked/shell/train_bias_agents/train_bias_agents_sep.sh
@@ -6,7 +6,7 @@ weight_pattern=$2
 
 entropy_coefs="0.2 0.05 0.001"
 entropy_coef_horizons="0 6e6 1e7"
-reward_shaping_horizon="0.5e7"
+reward_shaping_horizon="1e7"
 num_env_steps="1e7"
 
 if [[ "${layout}" == "small_corridor" ]]; then

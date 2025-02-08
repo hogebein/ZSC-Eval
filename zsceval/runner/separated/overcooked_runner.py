15dd983d0675ce9e3f1b289a149174b835de157c
Ta183 <47871855+hogebein@users.noreply.github.com>
2025-02-09 02:18:54 +0900

    update

--- zsceval/runner/separated/overcooked_runner.py
+++ zsceval/runner/separated/overcooked_runner.py
@@ -42,7 +42,7 @@ class OvercookedRunner(Runner):
                     rnn_states_critic,
                 ) = self.collect(step)
 
-                logger.debug(actions)
+                #logger.debug(actions)
 
                 if self.all_args.random_index == False and self.all_args.fixed_index==1:
                     actions[0][0], actions[0][1] = actions[0][1], actions[0][0]
--- zsceval/scripts/overcooked/shell/train_bias_agents/train_bias_agents_shared.sh
+++ zsceval/scripts/overcooked/shell/train_bias_agents/train_bias_agents_shared.sh
@@ -172,7 +172,7 @@ do
         --seed ${seed} --n_training_threads 2 --n_rollout_threads ${rollout_threads} --dummy_batch_size 2 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
         --overcooked_version ${version} \
         --ppo_epoch 15 --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
-        --use_hsp --w0 ${w0} --w1 ${w1} --random_index \
+        --use_hsp --w0 ${w0} --w1 ${w1} \
         --cnn_layers_params "32,3,1,1 64,3,1,1 32,3,1,1" --use_recurrent_policy \
         --use_proper_time_limits \
         --save_interval 25 --log_interval 10 --use_eval --eval_interval 20 --n_eval_rollout_threads 20 \
@@ -190,7 +190,7 @@ do
         --seed ${seed} --n_training_threads 2 --n_rollout_threads ${rollout_threads} --dummy_batch_size 2 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
         --overcooked_version ${version} \
         --ppo_epoch 15 --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
-        --use_hsp --w0 ${w0} --w1 ${w1} --random_index \
+        --use_hsp --w0 ${w0} --w1 ${w1} \
         --cnn_layers_params "32,3,1,1 64,3,1,1 32,3,1,1" --use_recurrent_policy \
         --use_proper_time_limits \
         --save_interval 25 --log_interval 10 --use_eval --eval_interval 20 --n_eval_rollout_threads 20 \
@@ -207,7 +207,7 @@ do
         --seed ${seed} --n_training_threads 2 --n_rollout_threads ${rollout_threads} --dummy_batch_size 2 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
         --overcooked_version ${version} \
         --ppo_epoch 15 --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
-        --use_hsp --w0 ${w0} --w1 ${w1} --random_index \
+        --use_hsp --w0 ${w0} --w1 ${w1} \
         --cnn_layers_params "32,3,1,1 64,3,1,1 32,3,1,1" --use_recurrent_policy \
         --use_proper_time_limits \
         --save_interval 25 --log_interval 10 --use_eval --eval_interval 20 --n_eval_rollout_threads 20 \

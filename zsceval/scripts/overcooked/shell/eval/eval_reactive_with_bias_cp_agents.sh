682d31c995c2e87bd10b79a778a8ff453d8a7364
hogebein <gtx1070@DESKTOP-24HB6O7>
2025-03-19 20:35:30 +0900

    added file

---
+++ zsceval/scripts/overcooked/shell/eval/eval_reactive_with_bias_cp_agents.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+env="Overcooked"
+
+layout=$1
+
+if [[ "${layout}" == "random0" || "${layout}" == "random0_medium" || "${layout}" == "random1" || "${layout}" == "random3" || "${layout}" == "small_corridor" || "${layout}" == "unident_s" ]]; then
+    version="old"
+else
+    version="new"
+fi
+
+num_agents=2
+algo="population"
+
+if [[ $2 == "fcp" ]];
+then
+    algorithm="fcp"
+    # exps=("fcp-S2-s12")
+    exps=("fcp-S2-s24" "fcp-S2-s36")
+    # exps=("fcp-S2-s12" "fcp-S2-s24" "fcp-S2-s36")
+    exps=("fcp-S2-s24" "fcp-S2-s36")
+elif [[ $2 == "mep" ]];
+then
+    algorithm="mep"
+    # exps=("mep-S2-s12")
+    exps=("mep-S2-s24" "mep-S2-s36")
+    # exps=("mep-S2-s12" "mep-S2-s24" "mep-S2-s36")
+    exps=("mep-S2-s24" "mep-S2-s36")
+elif [[ $2 == "traj" ]];
+then
+    algorithm="traj"
+    exps=("traj-S2-s24" "traj-S2-s36")
+    # exps=("traj-S2-s12" "traj-S2-s24" "traj-S2-s36")
+    exps=("traj-S2-s24" "traj-S2-s36")
+elif [[ $2 == "hsp" ]];
+then
+    algorithm="hsp"
+    # exps=("hsp-S2-s24" "hsp-S2-s36")
+    exps=("hsp-S2-s12" "hsp-S2-s24" "hsp-S2-s36")
+elif [[ $2 == "cole" ]];
+then
+    algorithm="cole"
+    exps=("cole-S2-s50" "cole-S2-s75")
+    # exps=("cole-S2-s25" "cole-S2-s50" "cole-S2-s75")
+    exps=("cole-S2-s50" "cole-S2-s75")
+elif [[ $2 == "hsp_cp" ]];
+then
+    algorithm="hsp_cp"
+    #exps=("adaptive_hsp_plate_shared-pop_cp-s60" "hsp_plate_shared-pop_cp-s60")
+    #exps=("mep-S2-s36-adp_cp-s5" "hsp_plate-S2-s36-adp_cp-s5" "adaptive_mep-S2-s36-adp_cp-s5" "adaptive_hsp_plate-S2-s36-adp_cp-s5")
+
+elif [[ $2 == "hsp_react" ]];
+then
+    algorithm="hsp"
+    exps=("hsp_plate_placement_shared")
+    
+
+else
+    echo "bash eval_with_bias_agents.sh {layout} {algo}"
+    exit 0
+fi
+
+pop_agent_version="hsp_plate_placement_shared-S2-s12"
+pop_agent_algo="hsp"
+
+declare -A LAYOUTS_KS
+LAYOUTS_KS["random0"]=10
+LAYOUTS_KS["random0_medium"]=10
+LAYOUTS_KS["random1"]=10
+LAYOUTS_KS["random3"]=10
+LAYOUTS_KS["small_corridor"]=10
+LAYOUTS_KS["unident_s"]=10
+LAYOUTS_KS["random0_m"]=15
+LAYOUTS_KS["random1_m"]=15
+LAYOUTS_KS["random3_m"]=5
+
+path=../../policy_pool
+export POLICY_POOL=${path}
+
+K=$((LAYOUTS_KS[${layout}]))
+bias_yml="${path}/${layout}/${pop_agent_algo}/s2/${pop_agent_version}/benchmarks-s${K}.yml"
+yml_dir=eval/eval_policy_pool/${layout}/results
+mkdir -p ${yml_dir}
+
+n=$(grep -o -E 'bias.*_(final|mid):' ${bias_yml} | wc -l)
+echo "Evaluate ${layout}, ${exps} with ${n} agents"
+population_size=$((n + 1))
+
+ulimit -n 65536
+
+len=${#exps[@]}
+for (( i=0; i<$len; i++ )); do
+    exp=${exps[$i]}
+
+    echo "Evaluate population ${algo} ${exp} ${population}"
+    for seed in $(seq 1 10); do
+        exp_name="${exp}"
+        agent_name="${exp_name}-${seed}"
+        
+        echo "Exp name ${exp_name}"
+        eval_exp="eval_cp-${agent_name}"
+        yml=${yml_dir}/${eval_exp}.yml
+
+        w0_path="${path}/${layout}/${algorithm}/s1/${exp}/w0.json"
+        w0=$(cat ${w0_path} | jq ".${algorithm}${seed}_final_actor")
+        
+        sed -e "s/agent_name/${agent_name}/g" -e "s/algorithm/${algorithm}/g" -e "s/population/${exp_name}/g" -e "s/seed/hsp${seed}_final_actor/g" -e "s/@@@/${w0}/g" "${bias_yml}" > "${yml}"
+        
+        python eval/eval_with_population.py --env_name ${env} --algorithm_name ${algo} --experiment_name "${eval_exp}" --layout_name "${layout}" \
+        --num_agents ${num_agents} --seed 1 --episode_length 400 --n_eval_rollout_threads 1 --eval_episodes $((${n} * 40)) --eval_stochastic --dummy_batch_size 1 \
+        --use_proper_time_limits \
+        --use_wandb \
+        --store_traj \
+        --use_render \
+        --population_yaml_path "${yml}" --population_size ${population_size} \
+        --overcooked_version ${version} --eval_result_path "eval/results/${layout}/${algorithm}/${eval_exp}.json" \
+        --agent_name "${agent_name}" \
+        --use_agent_policy_id \
+        --use_opponent_utility
+    done
+done
---
+++ zsceval/scripts/overcooked/shell/train_hsp_stage_2_utility_side2.sh
@@ -0,0 +1,170 @@
+
+#!/bin/bash
+env="Overcooked"
+
+layout=$1
+population_size=$2
+index=$3
+pop_version=$4
+
+if [[ "${layout}" == "random0" || "${layout}" == "random0_medium" || "${layout}" == "random1" || "${layout}" == "random3" || "${layout}" == "small_corridor" || "${layout}" == "unident_s" ]]; then
+    version="old"
+else
+    version="new"
+fi
+
+use_base_shaping_r=true
+
+if [[ ${population_size} == 5 ]]; then
+    entropy_coefs="0.2 0.05 0.01"
+    entropy_coef_horizons="0 2.5e7 5e7"
+    if [[ ${pop_version} == "tomato_delivery" ]]; then
+        pop="hsp_tomato_delivery"
+        reward_shaping_horizon="15e7"
+        num_env_steps="15e7"
+        use_base_shaping_r=true
+    elif [[ ${pop_version} == "tomato_lover" ]]; then
+        pop="hsp_onion_tomato"
+        reward_shaping_horizon="5e7"
+        num_env_steps="5e7"
+        use_base_shaping_r=true
+    elif [[ ${pop_version} == "score" ]]; then
+        reward_shaping_horizon="5e7"
+        num_env_steps="5e7"
+        pop="hsp_score"
+        use_base_shaping_r=true
+    else
+        pop="hsp_plate_placement"
+        reward_shaping_horizon="3e7"
+        num_env_steps="3e7"
+        use_base_shaping_r=true
+    fi
+    mep_exp="no_mep"
+
+elif [[ ${population_size} == 10 ]]; then
+    entropy_coefs="0.2 0.05 0.01"
+    entropy_coef_horizons="0 2.5e7 5e7"
+
+    if [[ ${pop_version} == "tomato_delivery" ]]; then
+        pop="hsp_tomato_delivery"
+        reward_shaping_horizon="15e7"
+        num_env_steps="15e7"
+        use_base_shaping_r=true
+    elif [[ ${pop_version} == "onion_tomato" ]]; then
+        pop="hsp_onion_tomato"
+        reward_shaping_horizon="5e7"
+        num_env_steps="5e7"
+        use_base_shaping_r=true
+    else
+        pop="hsp_plate_placement"
+        reward_shaping_horizon="10e7"
+        num_env_steps="10e7"
+        use_base_shaping_r=true
+    fi
+    mep_exp="no_mep"
+    
+elif [[ ${population_size} == 12 ]]; then
+    entropy_coefs="0.2 0.05 0.01"
+    entropy_coef_horizons="0 4e7 8e7"
+    if [[ "${layout}" == "small_corridor" ]]; then
+        entropy_coefs="0.2 0.05 0.01"
+        entropy_coef_horizons="0 6.4e7 8e7"
+    fi
+    reward_shaping_horizon="8e7"
+    num_env_steps="8e7"
+    pop="hsp_plate_placement"
+    mep_exp="mep-S1-s10"
+
+elif [[ ${population_size} == 24 ]]; then
+    entropy_coefs="0.2 0.05 0.01"
+    entropy_coef_horizons="0 4e7 8e7"
+    if [[ "${layout}" == "small_corridor" ]]; then
+        entropy_coefs="0.2 0.05 0.01"
+        entropy_coef_horizons="0 6.4e7 8e7"
+    fi
+    reward_shaping_horizon="8e7"
+    num_env_steps="8e7"
+    pop="hsp"
+    mep_exp="mep-S1-s10"
+elif [[ ${population_size} == 36 ]]; then
+    entropy_coefs="0.2 0.05 0.01"
+    entropy_coef_horizons="0 5e7 1e8"
+    if [[ "${layout}" == "small_corridor" ]]; then
+        entropy_coefs="0.2 0.05 0.01"
+        entropy_coef_horizons="0 8e7 1e8"
+    fi
+    reward_shaping_horizon="1e8"
+    num_env_steps="1e8"
+    pop="hsp"
+    mep_exp="mep-S1-s15"
+else
+    entropy_coefs="0.2 0.05 0.01"
+    entropy_coef_horizons="0 5e7 1e8"
+    if [[ "${layout}" == "small_corridor" ]]; then
+        entropy_coefs="0.2 0.05 0.01"
+        entropy_coef_horizons="0 8e7 1e8"
+    fi
+    reward_shaping_horizon="1e8"
+    num_env_steps="1e8"
+    pop="hsp"
+    mep_exp="mep-S1-s10"
+fi
+
+
+num_agents=2
+algo="adaptive"
+exp="v2-${pop}-S2-s${population_size}"
+stage="S2"
+seed_begin=1
+seed_max=5
+path=../../policy_pool
+
+export POLICY_POOL=${path}
+
+n_training_threads=200
+cuda=1
+
+ulimit -n 65536
+
+echo "env is ${env}, layout is ${layout}, algo is ${algo}, pop is ${pop}, exp is ${exp}, seed from ${seed_begin} to ${seed_max}, stage is ${stage}"
+for seed in $(seq ${seed_begin} ${seed_max});
+# for seed in 1 2 5;
+do
+
+    if "${use_base_shaping_r}"; then
+
+    	python train/train_adaptive.py --env_name ${env} --algorithm_name ${algo} --experiment_name "${exp}" --layout_name ${layout} --num_agents ${num_agents} \
+    	--seed ${seed} --n_training_threads 1 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
+    	--overcooked_version ${version} \
+    	--n_rollout_threads ${n_training_threads} --dummy_batch_size 1 \
+    	--ppo_epoch 15 --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
+    	--stage 2 \
+    	--save_interval 25 --log_interval 1 --use_eval --eval_interval 20 --n_eval_rollout_threads $((population_size * 2)) --eval_episodes 20 \
+    	--population_yaml_path ${path}/${layout}/hsp_react/s3/train-s${population_size}-${pop}_${mep_exp}-${seed}.yml \
+    	--population_size ${population_size} --adaptive_agent_name hsp_adaptive --use_agent_policy_id \
+    	--use_proper_time_limits \
+    	--wandb_name "hogebein" \
+    	--use_opponent_utility \
+        --use_base_shaping_r \
+        --fixed_index ${index} \
+    	--cuda_id ${cuda}
+
+    else 
+
+    	python train/train_adaptive.py --env_name ${env} --algorithm_name ${algo} --experiment_name "${exp}" --layout_name ${layout} --num_agents ${num_agents} \
+    	--seed ${seed} --n_training_threads 1 --num_mini_batch 1 --episode_length 400 --num_env_steps ${num_env_steps} --reward_shaping_horizon ${reward_shaping_horizon} \
+    	--overcooked_version ${version} \
+    	--n_rollout_threads ${n_training_threads} --dummy_batch_size 1 \
+    	--ppo_epoch 15 --entropy_coefs ${entropy_coefs} --entropy_coef_horizons ${entropy_coef_horizons} \
+    	--stage 2 \
+    	--save_interval 25 --log_interval 1 --use_eval --eval_interval 20 --n_eval_rollout_threads $((population_size * 2)) --eval_episodes 20 \
+    	--population_yaml_path ${path}/${layout}/hsp_react/s3/train-s${population_size}-${pop}_${mep_exp}-${seed}.yml \
+    	--population_size ${population_size} --adaptive_agent_name hsp_adaptive --use_agent_policy_id \
+    	--use_proper_time_limits \
+    	--wandb_name "hogebein" \
+    	--use_opponent_utility \
+        --fixed_index ${index} \
+	    --cuda_id ${cuda}
+    fi
+done
+
